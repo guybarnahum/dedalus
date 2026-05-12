62e237e9de9ade0379d1eda409a34f24a502f1d3
guybarnahum <guybarnahum@users.noreply.github.com>
2026-05-12 09:40:43 -0700

    feat(runtime): add pipeline timing writer to runner

--- include/dedalus/runtime/core_stack_runner.hpp
+++ include/dedalus/runtime/core_stack_runner.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <memory>
+
+#include "dedalus/runtime/pipeline_profiler.hpp"
 #include "dedalus/runtime/provider_registry.hpp"
 #include "dedalus/world_model/world_snapshot.hpp"
 
@@ -8,12 +11,14 @@ namespace dedalus {
 class CoreStackRunner {
 public:
     explicit CoreStackRunner(CoreStackProviders providers);
+    CoreStackRunner(CoreStackProviders providers, std::unique_ptr<PipelineProfiler> timing_writer);
 
     [[nodiscard]] bool run_once();
     [[nodiscard]] WorldSnapshot snapshot() const;
 
 private:
     CoreStackProviders providers_;
+    std::unique_ptr<PipelineProfiler> timing_writer_;
 };
 
 }  // namespace dedalus
