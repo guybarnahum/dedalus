02dfad8c5a509d022dd0217cce9703e07c5cb515
guybarnahum <guybarnahum@users.noreply.github.com>
2026-05-14 13:55:56 -0700

    feat(runtime): add async frame prefetch state

--- include/dedalus/runtime/core_stack_runner.hpp
+++ include/dedalus/runtime/core_stack_runner.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <future>
 #include <memory>
+#include <optional>
 
 #include "dedalus/runtime/pipeline_profiler.hpp"
 #include "dedalus/runtime/provider_registry.hpp"
@@ -12,13 +14,23 @@ class CoreStackRunner {
 public:
     explicit CoreStackRunner(CoreStackProviders providers);
     CoreStackRunner(CoreStackProviders providers, std::unique_ptr<PipelineProfiler> timing_writer);
+    ~CoreStackRunner();
+
+    CoreStackRunner(const CoreStackRunner&) = delete;
+    CoreStackRunner& operator=(const CoreStackRunner&) = delete;
+    CoreStackRunner(CoreStackRunner&&) = delete;
+    CoreStackRunner& operator=(CoreStackRunner&&) = delete;
 
     [[nodiscard]] bool run_once();
     [[nodiscard]] WorldSnapshot snapshot() const;
 
 private:
+    std::optional<FramePacket> fetch_next_frame();
+    void start_prefetch();
+
     CoreStackProviders providers_;
     std::unique_ptr<PipelineProfiler> timing_writer_;
+    std::future<std::optional<FramePacket>> prefetched_frame_;
 };
 
 }  // namespace dedalus
