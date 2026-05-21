9607b7a5ad13450d5b33e273d3dd61ba6077d315
guybarnahum <guybarnahum@users.noreply.github.com>
2026-05-20 17:48:31 -0700

    feat(runtime): publish snapshots through world snapshot publisher

--- include/dedalus/runtime/core_stack_runner.hpp
+++ include/dedalus/runtime/core_stack_runner.hpp
@@ -4,10 +4,10 @@
 #include <memory>
 #include <optional>
 
-#include "dedalus/behavior/latest_world_snapshot.hpp"
 #include "dedalus/runtime/pipeline_profiler.hpp"
 #include "dedalus/runtime/provider_registry.hpp"
 #include "dedalus/world_model/world_snapshot.hpp"
+#include "dedalus/world_model/world_snapshot_publisher.hpp"
 
 namespace dedalus {
 
@@ -18,7 +18,7 @@ public:
     CoreStackRunner(
         CoreStackProviders providers,
         std::unique_ptr<PipelineProfiler> timing_writer,
-        std::shared_ptr<LatestWorldSnapshot> latest_snapshot);
+        std::shared_ptr<WorldSnapshotPublisher> snapshot_publisher);
     ~CoreStackRunner();
 
     CoreStackRunner(const CoreStackRunner&) = delete;
@@ -35,7 +35,7 @@ private:
 
     CoreStackProviders providers_;
     std::unique_ptr<PipelineProfiler> timing_writer_;
-    std::shared_ptr<LatestWorldSnapshot> latest_snapshot_;
+    std::shared_ptr<WorldSnapshotPublisher> snapshot_publisher_;
     std::future<std::optional<FramePacket>> prefetched_frame_;
     std::optional<TimePoint> ghost_scenario_start_;
 };
