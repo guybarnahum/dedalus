1d7fdb9b3f8566de9e10565da1c6cceb22e77743
guybarnahum <guybarnahum@users.noreply.github.com>
2026-05-20 18:34:50 -0700

    feat(runtime): add reusable typed pubsub service

---
+++ include/dedalus/runtime/pubsub.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <memory>
+#include <mutex>
+#include <stdexcept>
+#include <utility>
+#include <vector>
+
+namespace dedalus {
+
+template <typename EventT>
+class Subscriber {
+public:
+    virtual ~Subscriber() = default;
+    virtual void on_event(const EventT& event) = 0;
+};
+
+template <typename EventT>
+class Publisher final {
+public:
+    using SubscriberType = Subscriber<EventT>;
+
+    void subscribe(std::shared_ptr<SubscriberType> subscriber) {
+        if (!subscriber) {
+            throw std::invalid_argument("Publisher::subscribe requires a subscriber");
+        }
+        std::lock_guard<std::mutex> lock{mutex_};
+        subscribers_.push_back(std::move(subscriber));
+    }
+
+    void publish(const EventT& event) {
+        std::vector<std::shared_ptr<SubscriberType>> subscribers;
+        {
+            std::lock_guard<std::mutex> lock{mutex_};
+            auto output = subscribers_.begin();
+            for (auto input = subscribers_.begin(); input != subscribers_.end(); ++input) {
+                if (auto subscriber = input->lock()) {
+                    subscribers.push_back(subscriber);
+                    *output++ = *input;
+                }
+            }
+            subscribers_.erase(output, subscribers_.end());
+        }
+
+        for (const auto& subscriber : subscribers) {
+            subscriber->on_event(event);
+        }
+    }
+
+    [[nodiscard]] std::size_t subscriber_count() const {
+        std::lock_guard<std::mutex> lock{mutex_};
+        std::size_t count = 0;
+        for (const auto& subscriber : subscribers_) {
+            if (!subscriber.expired()) {
+                ++count;
+            }
+        }
+        return count;
+    }
+
+private:
+    mutable std::mutex mutex_;
+    std::vector<std::weak_ptr<SubscriberType>> subscribers_;
+};
+
+}  // namespace dedalus
