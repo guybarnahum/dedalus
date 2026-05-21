9bcaa3809d91c3d656a5ada9d7b39ea23828ffc7
guybarnahum <guybarnahum@users.noreply.github.com>
2026-05-20 18:35:20 -0700

    refactor(world): make snapshot publisher use generic pubsub

--- include/dedalus/world_model/world_snapshot_publisher.hpp
+++ include/dedalus/world_model/world_snapshot_publisher.hpp
@@ -1,27 +1,11 @@
 #pragma once
 
-#include <memory>
-#include <mutex>
-#include <vector>
-
+#include "dedalus/runtime/pubsub.hpp"
 #include "dedalus/world_model/world_snapshot.hpp"
 
 namespace dedalus {
 
-class WorldSnapshotSubscriber {
-public:
-    virtual ~WorldSnapshotSubscriber() = default;
-    virtual void on_snapshot(const WorldSnapshot& snapshot) = 0;
-};
-
-class WorldSnapshotPublisher final {
-public:
-    void subscribe(std::shared_ptr<WorldSnapshotSubscriber> subscriber);
-    void publish(const WorldSnapshot& snapshot);
-
-private:
-    mutable std::mutex mutex_;
-    std::vector<std::weak_ptr<WorldSnapshotSubscriber>> subscribers_;
-};
+using WorldSnapshotSubscriber = Subscriber<WorldSnapshot>;
+using WorldSnapshotPublisher = Publisher<WorldSnapshot>;
 
 }  // namespace dedalus
